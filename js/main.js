540842cd424ca4c6e5b82eed6e49eddd92d4c77b
prakhardoneria <gamesprotec@gmail.com>
2024-11-30 01:00:51 +0530

    production

--- js/main.js
+++ js/main.js
@@ -1,29 +1,28 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const moviesGrid = document.getElementById("trending-movies");
-  
-    // Fetch trending movies
-    try {
-      const response = await fetch("https://movies-backend-api.vercel.app/trending?country=IN");
-      const data = await response.json();
-  
-      data.trending.forEach((movie) => {
-        const movieCard = document.createElement("div");
-        movieCard.classList.add("movie-card");
-        movieCard.innerHTML = `
-          <img src="${movie.thumbnail}" alt="${movie.title}">
-          <h3>${movie.title}</h3>
-          <p>${movie.views} views</p>
-          <a href="${movie.video_url}" target="_blank">Watch Now</a>
-        `;
-        moviesGrid.appendChild(movieCard);
-      });
-    } catch (error) {
-      console.error("Error fetching trending movies:", error);
-    }
-  
-    // Navigate to search page
-    document.getElementById("search-button").addEventListener("click", () => {
-      window.location.href = "search.html";
+  const moviesGrid = document.getElementById("trending-movies");
+
+  // Fetch trending movies
+  try {
+    const response = await fetch("https://movies-backend-api.vercel.app/trending?country=IN");
+    const data = await response.json();
+
+    data.trending.forEach((movie) => {
+      const movieCard = document.createElement("div");
+      movieCard.classList.add("movie-card");
+      movieCard.innerHTML = `
+        <img src="${movie.thumbnail}" alt="${movie.title}">
+        <h3>${movie.title}</h3>
+        <p>${movie.views} views</p>
+        <a href="${movie.video_url}" target="_blank">Watch Now</a>
+      `;
+      moviesGrid.appendChild(movieCard);
     });
+  } catch (error) {
+    console.error("Error fetching trending movies:", error);
+  }
+
+  // Navigate to search page
+  document.getElementById("search-button").addEventListener("click", () => {
+    window.location.href = "search.html";
   });
-  
+});
